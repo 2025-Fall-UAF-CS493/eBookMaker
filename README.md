4107c8bdcee8c31b2f062dc628328f3473fba2e1
Lane Schwartz <dowobeha@gmail.com>
2025-10-11 12:36:34 -0800

    Sample code to render pdf using flutter pdfx library

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # eBookMaker
+
+Libraries:
+* [pdfrx](https://pub.dev/packages/pdfrx)
+* [pdfx](https://pub.dev/packages/pdfx)
+
+Links:
+* [Flutter: how to display pdf files in your app using pdfx](https://medium.com/@anna_muzykina/flutter-how-to-display-pdf-files-in-your-app-using-package-pdfx-the-old-version-of-this-package-c5e54f058db0)
--- lib/widgets/homepage.dart
+++ lib/widgets/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pdfx/pdfx.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key, required this.title});
@@ -10,39 +11,22 @@ class Homepage extends StatefulWidget {
 }
 
 class HomepageContent extends State<Homepage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  late final PdfController pdfController;
+
+  @override
+  void initState() {
+    super.initState();
+    // Use PdfDocument.openAsset for bundled asset.
+    pdfController = PdfController(
+      document: PdfDocument.openAsset('assets/sample.pdf'),
+    );
   }
+
 
   @override
   Widget build(BuildContext context) {
 
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+    return PdfView(controller: pdfController);
   }
 }
